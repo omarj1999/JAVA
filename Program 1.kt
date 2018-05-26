c710ca7834f5bef4fd356cc7c2a3c468da731ed4
عمر <39658097+omarj1999@users.noreply.github.com>
2018-05-27 04:15:35 +0700

    Add files via upload

---
+++ Program 1.kt
@@ -0,0 +1,9 @@
+//start//
+
+fun main (args:Array<String>)
+{
+
+  var om="omar"
+    println(om)
+
+}
---
+++ Program 2.kt
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const
+/////START PROGRAM//////////
+fun main (args:Array<String>)
+{
+    val x= readLine()
+    println("wellcome"+x)
+
+
+}
+/////////END PROGRAM/////////
